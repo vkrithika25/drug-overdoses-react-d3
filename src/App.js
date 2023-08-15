e2c261fdbed2e404a83cb3e25989f15355367372
vkrithika25 <v.krithika@gmail.com>
2023-08-14 20:04:35 -0700

    remove junk

--- src/App.js
+++ src/App.js
@@ -7,61 +7,14 @@ import BoxPage from './components/BoxPage';
 import ScatterplotPage from './components/ScatterplotPage';
 
 export default function App() {
-    // const margins = {
-    //     left: 35,
-    //     right: 35,
-    //     top: 10,
-    //     bottom: 35
-    // };
-    // const dims = {
-    //     height: 575,
-    //     width: 650  
-    // };
-
-    // const map_margins = {
-    //   left: 35,
-    //   right: 35,
-    //   top: 10,
-    //   bottom: 35
-    // };
-    // const map_dims = {
-    //     height: 500,
-    //     width: 750
-    // };
-
-    // const [selectedSort, setSelectedSort] = useState('alphabetical');
-
-    // function handleSortingMethodChange(sort) {
-    //   setSelectedSort(sort);
-    // }
-
-    // const [selectedColors, setSelectedColors] = useState(['FACILITY', 'SYRINGE']);
-    
-    // useEffect(() => {
-    //   console.log("Updated selected colors:", selectedColors);
-    // }, [selectedColors]);
-
-    
-    // function handleFilterItemClick(color) {
-    //   if (selectedColors.includes(color)) {
-    //     var newColors = selectedColors.filter((d) => d !== color);
-    //     setSelectedColors(newColors);
-    //     if (newColors.length === 0) {
-    //       (color === 'FACILITY') ? setSelectedColors(['SYRINGE']) : setSelectedColors(['FACILITY'])
-    //     }
-    //   } else {
-    //     setSelectedColors([...selectedColors, color]);
-    //   }
-    // }
-
     return (
       <div>
       <Router>
             <Routes>
-                <Route exact path='/drug-overdoses-react-d3' element={<HomePage />} />
-                <Route path='/drug-overdoses-react-d3/scatterplot' element={<ScatterplotPage />} />
-                <Route path='/drug-overdoses-react-d3/map' element={<MapPage />} />
-                <Route path='/drug-overdoses-react-d3/box' element={<BoxPage />} />
+                <Route exact path='/' element={<HomePage />} />
+                <Route path='/scatterplot' element={<ScatterplotPage />} />
+                <Route path='/map' element={<MapPage />} />
+                <Route path='/box' element={<BoxPage />} />
             </Routes>
         </Router>
       </div>
